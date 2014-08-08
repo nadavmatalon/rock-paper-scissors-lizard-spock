aadec1d0bd3198cba814125e3b894bbce1262510
Nadav <nadav.matalon@gmail.com>
2014-08-08 12:16:49 +0100

    removed enable sessions from server.rb file

--- javascript/app/server.rb
+++ javascript/app/server.rb
@@ -3,8 +3,6 @@ require "sinatra"
 set :views, Proc.new {File.join(root, '..', "views")}
 set :public_folder, Proc.new {File.join(root, '..', "public")}
 
-enable :sessions
-
 get "/" do 
 	session[:message] = "hello"
 	erb :index
