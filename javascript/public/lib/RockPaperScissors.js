b54a4436c32fa0e6988502291d0b2c774124c6b3
Nadav <nadav.matalon@gmail.com>
2014-10-31 19:28:06 +0000

    split texts between two JS versions of the game

--- javascript/public/lib/RockPaperScissors.js
+++ javascript/public/lib/RockPaperScissors.js
@@ -1,7 +1,7 @@
 
 function Player(name) {
 	this.name = name;
-};
+}
 
 Player.prototype.picks = function(pick) {
 	this.pick = pick;
@@ -10,12 +10,12 @@ Player.prototype.picks = function(pick) {
 function Game(player1, player2) {
 	this.player1 = player1;
 	this.player2 = player2;
-};
+}
 
 Game.prototype.PAIRS = {
-	rock: {beats: 'scissors'},
-	paper: {beats: 'rock'},
-	scissors: {beats: 'paper'}
+	rock: {'beats': 'scissors'},
+	paper: {'beats': 'rock'},
+	scissors: {'beats': 'paper'}
 };
 
 Game.prototype._isSamePick = function() {
@@ -23,11 +23,11 @@ Game.prototype._isSamePick = function() {
 };
 
 Game.prototype.winner = function() {
-	if (this._isSamePick()) return null;	
+	if (this._isSamePick()) { return null; }
 	if (this.PAIRS[this.player1.pick]['beats'] === this.player2.pick) {
 		return this.player1;
 	} else {
 		return this.player2;
-	};
+	}
 };
 
