43527db7aed4ef0e56e696876604bd2af3ada049
Nadav <nadav.matalon@gmail.com>
2014-10-31 19:43:11 +0000

    code refactoring of JS functions

--- javascript/public/lib/RockPaperScissorsLizardSpock.js
+++ javascript/public/lib/RockPaperScissorsLizardSpock.js
@@ -1,7 +1,7 @@
 
 function Player(name) {
     this.name = name;
-};
+}
 
 Player.prototype.picks = function(pick) {
     this.pick = pick;
@@ -10,18 +10,18 @@ Player.prototype.picks = function(pick) {
 function Game(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
-};
+}
 
 Player.prototype.pick = function() {
     this.pick;
-}
+};
 
 Game.prototype.PAIRS = {
-    rock: {beats: ['scissors', 'lizard']},
-    paper: {beats: ['rock', 'spock']},
-    scissors: {beats: ['paper', 'lizard']},
-    lizard: {beats: ['paper', 'spock']},
-    spock: {beats: ['scissors', 'rock']}
+    rock:     { 'beats': ['scissors', 'lizard'] },
+    paper:    { 'beats': ['rock', 'spock']      },
+    scissors: { 'beats': ['paper', 'lizard']    },
+    lizard:   { 'beats': ['paper', 'spock']     },
+    spock:    { 'beats': ['scissors', 'rock']   }
 };
 
 Game.prototype._isSamePick = function() {
@@ -29,7 +29,7 @@ Game.prototype._isSamePick = function() {
 };
 
 Game.prototype.winner = function() {
-    if (this._isSamePick()) return null;
+    if (this._isSamePick()) { return null; }
     return (this.PAIRS[this.player1.pick]['beats'].indexOf(this.player2.pick) > -1) ? this.player1 : this.player2;
 };
 
@@ -42,7 +42,7 @@ Game.prototype.winnerName = function() {
         return this.player1.name;
     } else {
         return this.player2.name;
-    };
+    }
 };
 
 Game.prototype.loserName = function() {
@@ -52,31 +52,31 @@ Game.prototype.loserName = function() {
 Game.prototype.isDraw = function() {
     if(this.winner() === null) {
         return 'draw';
-    };
+    }
 };
 
 Game.prototype.messageVerb = function() {
     if (this.winnerPick('rock') && this.loserPick('scissors')) {
-        return 'crushes'
+        return 'crushes';
     } else if (this.winnerPick('rock') && this.loserPick('lizard')) {
-        return 'crushes'
+        return 'crushes';
     } else if (this.winnerPick('scissors') && this.loserPick('paper')) {
-        return 'cuts'
+        return 'cuts';
     } else if (this.winnerPick('scissors') && this.loserPick('lizard')) {
-        return 'decapitates'
+        return 'decapitates';
     } else if (this.winnerPick('spock') && this.loserPick('scissors')) {
-        return 'smashes'
+        return 'smashes';
     } else if (this.winnerPick('spock') && this.loserPick('rock')) {
-        return 'vapourises'
+        return 'vapourises';
     } else if (this.winnerPick('lizard') && this.loserPick('paper')) {
-        return 'eats'
+        return 'eats';
     } else if (this.winnerPick('lizard') && this.loserPick('spock')) {
-        return 'poisons'
+        return 'poisons';
     } else if (this.winnerPick('paper') && this.loserPick('rock')) {
-        return 'covers'
+        return 'covers';
     } else if (this.winnerPick('paper') && this.loserPick('spock')) {
-        return 'disproves'
-    };
+        return 'disproves';
+    }
 };
 
 Game.prototype.winnerPick = function(selection) {
@@ -88,7 +88,7 @@ Game.prototype.loserPick = function(selection) {
 };
 
 Game.prototype.winningMessage = function() {
-    if (this.isDraw()) return "Draw";
+    if (this.isDraw()) { return "Draw"; }
     var message = [this.winnerName(), this.messageVerb(), this.loserName()];
     return message.join(" ");
 };
